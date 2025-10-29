dde44ed137a9bda951b2cb3fc3e76bfeb6c3cee3
JoseManuelBarba <josemanuelbarbasanchez@gmail.com>
2025-10-29 12:25:04 +0100

    create layout

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ui_widgets_tree/utils/mock_data.dart';
+import 'package:ui_widgets_tree/widgets/section.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,75 +13,17 @@ class HomePage extends StatelessWidget {
         title: Text("Mi ARBNB"),
         actions: [Icon(Icons.menu)],
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [Text("Titulo"), Text("Navegación")],
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          ),
-          Expanded(
-            flex: 56,
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: Column(
-                        children: [Container(color: Colors.blue, height: 200)],
-                      ),
-                    ),
-                    Expanded(
-                      flex: 5,
-                      child: Column(
-                        children: [Container(color: Colors.black, height: 200)],
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Column(
-                      children: [
-                        Container(height: 200, width: 200, color: Colors.black),
-                        Container(height: 200, width: 200, color: Colors.blue),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Container(height: 200, width: 200, color: Colors.blue),
-                        Container(height: 200, width: 200, color: Colors.black),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Container(height: 200, width: 200, color: Colors.black),
-                        Container(height: 200, width: 200, color: Colors.blue),
-                      ],
-                    ),
-                  ],
-                ),
-
-                Expanded(
-                  child: GridView.builder(
-                    shrinkWrap: true,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 5,
-                        ),
-                    itemCount: 12,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Card(
-                        color: Colors.amber,
-                        child: Center(child: Text('$index')),
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.only(left: 10),
+        child: ListView.builder(
+          itemCount: MockData.sections.length,
+          itemBuilder: (context, index) {
+            return Section(
+              alojamientos: MockData.sections[index].accomations,
+              title: MockData.sections[index].title,
+            );
+          },
+        ),
       ),
     );
   }
